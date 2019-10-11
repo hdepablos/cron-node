c2dc8e71e670057f058ae57f0675b9c1fdeb3818
coleman <coleman@hidt>
2019-10-10 21:49:50 -0300

    Add una simple estructura

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const cron = require('node-cron');
+const { db, http, signos } = require("./util")
+
+class Main {
+    static async getData() {
+        const todayHoroscope = [];
+
+        for (const signo of signos) {
+            const { horoscope } = (await http.get('/' + signo)).data;
+            console.log('horoscope');
+            console.log(horoscope);
+            
+            todayHoroscope.push({
+                signo,
+                horoscope
+            })
+
+            console.log('today horoscope');
+            console.log(todayHoroscope);
+            
+
+            const name = `${new Date().toDateString().split(" ").join("_")}.json`;
+            db.setItem(name, JSON.stringify(todayHoroscope));
+
+
+        }
+    }
+}
+
+cron.schedule("* * * * * *", () => {
+    console.log('Se está ejecutando el cron');
+    Main.getData();
+});
---
+++ util/axios.js
@@ -0,0 +1,5 @@
+const axios = require('axios').default;
+
+module.exports = axios.create({
+    baseURL: "http://horoscope-api.herokuapp.com/horoscope/today/"
+})
